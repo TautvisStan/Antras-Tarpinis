770380c8cd69f90534dd81ffd521994c89521005
TautvisStan <tassat12321@gmail.com>
2025-03-18 17:18:37 +0200

    Pataisymai

--- routes/grupes_routes.py
+++ routes/grupes_routes.py
@@ -4,7 +4,7 @@ import services.grupes_actions as gr_act
 from forms.grupesForma import GrupesForma
 def init_grupes_routes(app):
     @app.route('/grupes')
-    def moduliai():
+    def grupes():
         return render_template('grupes.html', moduliai = gr_act.view_grupes())
 
     @app.route('/grupes_create', methods=['GET', 'POST'])
