ac5d8a6529a9a8192b97622d1193b068b8c48a08
TautvisStan <tassat12321@gmail.com>
2025-03-18 10:13:25 +0200

    modelis.py

--- models/modulis.py
+++ models/modulis.py
@@ -7,6 +7,6 @@ class Modulis(db.Model):
     aprasymas = db.Column(db.String(50), nullable=False)
     kreditai = db.Column(db.Integer, nullable=False)
     semestro_informacija = db.Column(db.String(50), nullable=False)
-    #studiju tvarkarastis
+
     studiju_programa_id = db.Column(db.Integer, db.ForeignKey('studiju_programos.id'), nullable=True)
     destytojas_id = db.Column(db.Integer, db.ForeignKey('vartotojai.id'), nullable=True)
