b3543dd9ad2408b3eb77c679b5ba2a774fa6961b
TautvisStan <tassat12321@gmail.com>
2025-03-19 11:39:04 +0200

    Pataisymai

--- routes/vartotojas_routes.py
+++ routes/vartotojas_routes.py
@@ -6,11 +6,11 @@ def init_student_routes(app):
     def students():
         return render_template('students.html', students=st_act.view_students())
 
-    @app.route('/create', methods=['GET', 'POST'])
-    def create():
-        return
+    # @app.route('/create', methods=['GET', 'POST'])
+    # def create():
+    #     return
     
     @app.route('/studentas_view/<id>', methods=['GET', 'POST'])
-    def view(id):
+    def view_studentas(id):
         studentas = st_act.gauti_studenta(id)
         return render_template('studento_perziura.html',studentas = studentas)
