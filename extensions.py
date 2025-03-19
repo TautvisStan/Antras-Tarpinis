e2cb3789ba161f36daf7ef269c32c3830e177848
TautvisStan <tassat12321@gmail.com>
2025-03-19 08:07:12 +0200

    pataisymai

--- extensions.py
+++ extensions.py
@@ -1,9 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask import Flask
 
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
-app = Flask(__name__)
