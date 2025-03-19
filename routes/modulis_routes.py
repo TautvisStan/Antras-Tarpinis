0b1109a30c7e425b55950360e4daf2422d37ca8b
ALUPEIKA <lupeika.algirdas@gmail.com>
2025-03-19 14:09:21 +0200

    foreign key priskyrimas

--- routes/modulis_routes.py
+++ routes/modulis_routes.py
@@ -1,6 +1,11 @@
 from flask import flash, render_template, request, url_for
+from flask_login import current_user
 import services.modulis_actions as mo_act
 from forms.modulisForma import ModulisForma
+from extensions import db
+from sqlalchemy import select
+from models.vartotojas import Vartotojas
+
 def init_modulis_routes(app):
     @app.route('/moduliai')
     def moduliai():
@@ -17,13 +22,17 @@ def init_modulis_routes(app):
                 aprasymas = form.aprasymas.data
                 kreditai = form.kreditai.data
                 semestro_informacija = form.semestro_informacija.data
-                mo_act.sukurti_moduli(pavadinimas, aprasymas, kreditai, semestro_informacija)
+
+                destytojas_id = current_user.id
+                studiju_programa = db.session.get(Vartotojas.id).studiju_programa_id
+
+                mo_act.sukurti_moduli(pavadinimas, aprasymas, kreditai, semestro_informacija, destytojas_id, studiju_programa)
                 flash("Sekmingai sukurta")
                 return app.redirect(url_for('moduliai'), 302)
             except Exception as e:
                 zinute = e
             return render_template("moduliai_forma.html", form=form)  
-    
+     
     @app.route('/moduliai_edit/<id>', methods=['GET', 'POST'])
     def update(id):
         modulis = mo_act.gauti_moduli(id)
@@ -54,4 +63,7 @@ def init_modulis_routes(app):
     def view(id):
         modulis = mo_act.gauti_moduli(id)
         return render_template('modulio_perziura.html',modulis=modulis)
+    
+
+
     
